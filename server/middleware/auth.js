1d76b0511358baceb804c9a8beefac8288304a98
Emrecan Balgün <deathlygrave@gmail.com>
2023-02-12 13:55:52 +0300

    add auth middleware, update app controller and route

---
+++ server/middleware/auth.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export default async function Auth(req, res, next) {
+  try {
+    // access authorization header to validate request
+    const token = req.headers.authorization.split(" ")[1];
+    
+    // retrive the user details fo the logged in user
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
+
+    res.json(decodedToken);
+  } catch (error) {
+    res.status(401).send({ error: "Authentication failed"})
+  }
+}
--- server/router/route.js
+++ server/router/route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import * as controller from "../controllers/appController.js";
+import Auth from "../middleware/auth.js";
 
 const router = Router();
 
@@ -17,7 +18,7 @@ router.route("/verifyOTP").get(controller.verifyOTP);
 router.route("createResetSession").get(controller.createResetSession);
 
 // PUT
-router.route("/updateUser").put(controller.updateUser);
+router.route("/updateUser").put(Auth, controller.updateUser);
 router.route("/resetPassword").put(controller.resetPassword);
 
 export default router;
